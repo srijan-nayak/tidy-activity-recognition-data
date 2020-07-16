1578ebe4d0586111c8bf0dc4af5251114dac41c3
Srijan Nayak <srijannayak0@gmail.com>
2020-07-16 15:28:10 +0530

    Read the x-train and x-test datasets
    These 2 datasets contains 561 measurements for each observation.

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,12 @@
+library(readr)
+
 # download and unzip the raw data
 download.file(
   "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
   destfile = "uci-har.zip"
 )
 unzip("./uci-har.zip")
+
+# read the x train and test sets
+x_train <- read_table2("UCI HAR Dataset/train/X_train.txt", col_names = FALSE)
+x_test <- read_table2("UCI HAR Dataset/test/X_test.txt", col_names = FALSE)
