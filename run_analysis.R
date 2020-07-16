492e92189a77c7c675b98e1ad41bc4e5fce87968
Srijan Nayak <srijannayak0@gmail.com>
2020-07-16 15:48:51 +0530

    Assign column names for x_train and x_test
    Read and extract the 561 feature names and assign them to column names for x_train and x_test.

--- run_analysis.R
+++ run_analysis.R
@@ -7,6 +7,13 @@ download.file(
 )
 unzip("./uci-har.zip")
 
+# read and extract the names of the 561 features
+features <- read_table2("./UCI HAR Dataset/features.txt",
+                        col_names = c("featurenum", "featurename"))
+feature_names <- features$featurename
+
 # read the x train and test sets
-x_train <- read_table2("./UCI HAR Dataset/train/X_train.txt", col_names = FALSE)
-x_test <- read_table2("./UCI HAR Dataset/test/X_test.txt", col_names = FALSE)
+x_train <- read_table2("./UCI HAR Dataset/train/X_train.txt",
+                       col_names = feature_names)
+x_test <- read_table2("./UCI HAR Dataset/test/X_test.txt",
+                      col_names = feature_names)
