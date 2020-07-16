44e3d297a028a13be19e4f8a263ea2ccb0947829
Srijan Nayak <srijannayak0@gmail.com>
2020-07-16 17:27:10 +0530

    Create subject column in x_train, x_test
    The subject column tells from which subject was a row of observation taken from.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+library(dplyr)
 library(readr)
 
 # download and unzip the raw data
@@ -17,3 +18,15 @@ x_train <- read_table2("./UCI HAR Dataset/train/X_train.txt",
                        col_names = feature_names)
 x_test <- read_table2("./UCI HAR Dataset/test/X_test.txt",
                       col_names = feature_names)
+
+# read in the subjects for each dataset
+subject_train <- read_lines("./UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read_lines("./UCI HAR Dataset/test/subject_test.txt")
+
+# create new subject column
+x_train <- x_train %>% 
+  mutate(subject = parse_integer(subject_train)) %>% 
+  select(subject, everything())
+x_test <- x_test %>% 
+  mutate(subject = parse_integer(subject_test)) %>% 
+  select(subject, everything())
