62f3e1970b5414973c5b76bc9b153e4ba1da1e48
Srijan Nayak <srijannayak0@gmail.com>
2020-07-16 10:30:05 +0530

    Update run_analysis.R a bit

---
+++ run_analysis.R
@@ -0,0 +1,6 @@
+# download and unzip the raw data
+download.file(
+  "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+  destfile = "uci-har.zip"
+)
+unzip("./uci-har.zip")
